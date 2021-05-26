57b4f6f0a0a1acc686d943bcc27182872279503f
john32313 <francois.jonathan13@gmail.com>
2021-05-26 17:32:05 +0200

    + header component with style sass

---
+++ src/component/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../assets/logo.svg';
+import burgerMenu from '../assets/burgermenu.svg';
+import './Header.scss';
+
+export default function Header() {
+  return (
+    <div className="Header">
+      <img src={logo} alt="logo Study Notes" />
+      <h3>
+        <span>Study</span>Notes
+      </h3>
+      <button type="button">
+        <img src={burgerMenu} alt="burgermenu" />
+      </button>
+    </div>
+  );
+}
