79a3e923724eafefb49b3655cf9cb6f817baace5
john32313 <francois.jonathan13@gmail.com>
2021-05-26 17:31:16 +0200

    + button component with style sass

---
+++ src/component/Button.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Button.scss';
+
+type PropsButton = {
+  title: string;
+  icon: string | null;
+  classname: string;
+};
+
+export default function Button({ title, icon, classname }: PropsButton) {
+  return (
+    <div className={`button ${classname}`}>
+      <h3>{title}</h3>
+      {icon && <img src={icon} alt={title} />}
+    </div>
+  );
+}
