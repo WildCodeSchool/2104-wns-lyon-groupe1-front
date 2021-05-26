22ddc0750e2621447fd4eae6a9e5d0f814922639
john32313 <francois.jonathan13@gmail.com>
2021-05-26 17:33:43 +0200

    + searchbar component with style sass

---
+++ src/component/SearchBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import search from '../assets/search.svg';
+import './SearchBar.scss';
+
+export default function SearchBar() {
+  return (
+    <div className="SearchBar">
+      <img src={search} alt={search} />
+    </div>
+  );
+}
