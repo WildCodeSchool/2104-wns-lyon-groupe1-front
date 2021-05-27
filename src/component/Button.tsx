c2a9e2474a474f901314d8f2503873fb13b38392
alteriade <groslambert@alteriade.fr>
2021-05-27 16:01:57 +0200

    + adding responsive + route slug for the different button

--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -1,16 +1,24 @@
+import { useHistory } from 'react-router-dom';
 import './Button.scss';
 
 type PropsButton = {
   title: string;
   icon: string | null;
   classname: string;
+  slug: string;
 };
 
-export default function Button({ title, icon, classname }: PropsButton) {
+export default function Button({ title, icon, classname, slug }: PropsButton) {
+  const history = useHistory();
+
   return (
-    <div className={`button ${classname}`}>
+    <button
+      type="button"
+      onClick={() => history.push(slug)}
+      className={`button ${classname}`}
+    >
       <h3>{title}</h3>
       {icon && <img src={icon} alt={title} />}
-    </div>
+    </button>
   );
 }
