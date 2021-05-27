431e8fd33c08d5d102eb358943d3c3b08773267e
alteriade <groslambert@alteriade.fr>
2021-05-27 16:12:38 +0200

    + added utils directory with interface.ts and UserContext.ts

---
+++ src/utils/UserContext.ts
@@ -0,0 +1,4 @@
+import { createContext } from 'react';
+import { IUser } from './interface';
+
+export default createContext<IUser>({});
---
+++ src/utils/interface.ts
@@ -0,0 +1,6 @@
+export interface IUser {
+  id?: string;
+  firstname?: string;
+  lastname?: string;
+  isTeacher?: boolean;
+}
