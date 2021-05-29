0ce6c5a894d1f606c583a77559e74a60439edf3a
john32313 <francois.jonathan13@gmail.com>
2021-05-29 20:40:38 +0200

    Move router and context to app, add connection page, and check if user is logged

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.scss';
+import { useState } from 'react';
 import Header from './component/Header';
 import Body from './component/Body';
 import Home from './route/Home';
@@ -7,32 +8,48 @@ import Forum from './route/Forum';
 import Study from './route/Study';
 import File from './route/File';
 import Footer from './component/Footer';
+import UserContext from './utils/UserContext';
+import { IUser } from './utils/interface';
 import ProfessorAccount from './route/ProfessorAccount';
+import Connect from './route/Connect';
 
 function App() {
+  const [user, setUser] = useState<IUser>({});
+
   return (
-    <div className="App">
-      <Header />
-      <Body>
-        <Switch>
-          <Route path="/mes-fiches-de-revisions" component={File} />
-          <Route
-            path="/nous-contacter"
-            render={() => <h2>Nous contacter</h2>}
-          />
-          <Route path="/notre-équipe" render={() => <h2>Notre équipe</h2>} />
-          <Route
-            path="/mentions-legales"
-            render={() => <h2>Mentions légales</h2>}
-          />
-          <Route path="/mes-matières" component={Study} />
-          <Route path="/forum" component={Forum} />
-          <Route path="/mon-espace" component={ProfessorAccount} />
-          <Route exact path="/" component={Home} />
-        </Switch>
-      </Body>
-      <Footer />
-    </div>
+    <Router>
+      <UserContext.Provider value={user}>
+        <div className="App">
+          <Header />
+          <Body>
+            <Switch>
+              <Route path="/mes-fiches-de-revisions" component={File} />
+              <Route
+                path="/nous-contacter"
+                render={() => <h2>Nous contacter</h2>}
+              />
+              <Route
+                path="/notre-équipe"
+                render={() => <h2>Notre équipe</h2>}
+              />
+              <Route
+                path="/mentions-legales"
+                render={() => <h2>Mentions légales</h2>}
+              />
+              <Route path="/mes-matières" component={Study} />
+              <Route path="/forum" component={Forum} />
+              <Route
+                path="/me-connecter"
+                render={() => <Connect setUser={setUser} />}
+              />
+              <Route path="/mon-espace" component={ProfessorAccount} />
+              <Route exact path="/" component={Home} />
+            </Switch>
+          </Body>
+          <Footer />
+        </div>
+      </UserContext.Provider>
+    </Router>
   );
 }
 
