59cc9a637e9a87677f9acd7abb8233cd09b93323
john32313 <francois.jonathan13@gmail.com>
2021-07-30 09:54:29 +0200

    - unuse variables

--- src/route/addPromotion.tsx
+++ src/route/addPromotion.tsx
@@ -22,7 +22,7 @@ export default function AddPromotion({ handleClassroom }: AddPromotionProps) {
     onCompleted: (value) => {
       handleClassroom(value.classroom);
     },
-    onError: (error) => {
+    onError: () => {
       // a changer, gerer les erreurs de retours créations classroom
       setPromotionNameError(true);
     },
