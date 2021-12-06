9e789be214f1b6535bcbf60d1afe747ade6bea09
sirageDb <ilovesemicolons@hotmail.com>
2021-12-06 14:58:51 +0100

    fix : switched to dev environnement user in index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -16,8 +16,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
-
-const httpLink = new HttpLink({ uri: "http://localhost:5000/" });
+const httpLink = new HttpLink({ uri: process.env.REACT_APP_API_URL });
+// const httpLink = new HttpLink({ uri: "http://localhost:5000/" });
 
 const authMidlw = new ApolloLink((operation, next) => {
   operation.setContext(({ headers = {} }) => ({
@@ -39,20 +39,20 @@ console.log(process.env);
 
 const CtxProvider = ({ children }: any) => {
   // PROD
-  const [user, addUser, removeUser] = useUserContext();
+  // const [user, addUser, removeUser] = useUserContext();
   // DEV
-  //  const [user, addUser, removeUser] = useUserContext({
-  //    id: '1',
-  //    firstname: 'John',
-  //    lastname: 'Doe',
-  //    isTeacher: false,
-  //    mail: 'nicolas.legrand@aze.com',
-  //    classroom: {
-  //      name: 'Développement web Lyon',
-  //      year: '2021/2022',
-  //      classroomId: '1',
-  //    },
-  //  });
+  const [user, addUser, removeUser] = useUserContext({
+     id: '1',
+     firstname: 'John',
+     lastname: 'Doe',
+     isTeacher: true,
+     mail: 'nicolas.legrand@aze.com',
+     classroom: {
+       name: 'Développement web Lyon',
+       year: '2021/2022',
+       classroomId: '1',
+     },
+   });
   return (
     <UserContext.Provider value={{ user, addUser, removeUser }}>
       {children}
