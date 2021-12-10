fd867d141da533bda0278eadb0e73bcc88ee4685
sirageDb <ilovesemicolons@hotmail.com>
2021-12-10 13:33:04 +0100

    + page title component

--- src/component/PageTitle.tsx
+++ src/component/PageTitle.tsx
@@ -5,10 +5,14 @@ interface IPageTitleProps {
 
 export default function PageTitle({ title, textColor }: IPageTitleProps) {
   return (
+<<<<<<< HEAD
     <h1
       className="customPageTitle"
       style={{ color: `${textColor}`, textAlign: 'center' }}
     >
+=======
+    <h1 className="customPageTitle" style={{ color: `${textColor}` }}>
+>>>>>>> d0b1243 (+ page title component)
       {title}
     </h1>
   );
