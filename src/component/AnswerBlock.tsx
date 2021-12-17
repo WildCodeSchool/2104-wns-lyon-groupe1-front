342d03a0396397bb9502cd2eb4206097e9550076
sirageDb <ilovesemicolons@hotmail.com>
2021-12-17 10:06:27 +0100

    + answer block component

--- src/component/AnswerBlock.tsx
+++ src/component/AnswerBlock.tsx
@@ -2,10 +2,20 @@ import { IAnswer } from '../utils/interface';
 import formattedDate from '../utils/dateFormatted';
 import './answerBlock.scss';
 
+<<<<<<< HEAD
 export default function AnswerBlock({ date, id, text }: IAnswer) {
   return (
     <div className="asnwerContainer">
       <div className="answerHeading">{formattedDate(date)}</div>
+=======
+export default function AnswerBlock({ author, date, id, text }: IAnswer) {
+  return (
+    <div className="asnwerContainer">
+      <div className="answerHeading">
+        <span>{author}</span>
+        <span>{formattedDate(date)}</span>
+      </div>
+>>>>>>> 8aaa6ce (+ answer block component)
       <p className="answerText">{text}</p>
     </div>
   );
